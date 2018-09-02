06d4c751ff059e95f7a4fba4a71aaf68f9fe2f28
github username <github email address>
2018-09-02 13:59:16 -0400

    Done.

--- index.js
+++ index.js
@@ -14,13 +14,3 @@ function setThisWithApply(fn, thisValue, args){
 function returnNewFunctionOf(functionToBeCopied, thisValue){
   return functionToBeCopied.bind(thisValue)
 }
-
-
-// function setThisWithApply(fn, thisValue, ourArguments) {
-//   return fn.apply(thisValue, ourArguments);
-// }
-//
-// function returnNewFunctionOf(functionToBeCopied, thisValue) {
-//   const copy = functionToBeCopied.bind(thisValue);
-//   return copy;
-// }
